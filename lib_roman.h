882eb3cd2381e35fdffb1f2d80f6264d29d7eaa7
R.J. Osborne <rosborne@pillartechnology.com>
2016-06-27 23:23:29 -0400

    added tricky case 1994

--- lib_roman.h
+++ lib_roman.h
@@ -1,9 +1,11 @@
 #ifndef _lib_roman_h
 #define _lib_roman_h
 
-#define TRANSLATION_SIZE 3
-char *proper_translation[TRANSLATION_SIZE] = { "IV", "XL", "XC" };
-char *simple_translation[TRANSLATION_SIZE] = {"IIII", "XXXX", "LXXXX"};
+#define TRANSLATION_SIZE 5
+char *proper_translation[TRANSLATION_SIZE] =
+ { "IV", "XL", "XC", "CD", "CM" };
+char *simple_translation[TRANSLATION_SIZE] =
+ {"IIII", "XXXX", "LXXXX", "CCCC", "DCCCC"};
 
 #define ROMAN_SIZE 7
 char *romans[ROMAN_SIZE] = { "M", "D", "C", "L", "X", "V", "I"};
